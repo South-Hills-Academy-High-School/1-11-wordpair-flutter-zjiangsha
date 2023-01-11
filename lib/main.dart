9b3ad476f19344f37d889afd3cad070266b3ef04
Ingrid <zjiangstudent@shacademy.org>
2023-01-11 12:19:19 -0800

    added like button

--- lib/main.dart
+++ lib/main.dart
@@ -60,12 +60,24 @@ class MyHomePage extends StatelessWidget {
           children: [
             Text('A random idea:'),
             BigCard(pair: pair),
-            ElevatedButton(
-              onPressed: () {
-                print('Hello');
-                appState.getNext();
-              },
-              child: Text('Next'),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ElevatedButton.icon(
+                  onPressed: (){
+                    appState.toggleFavourite();
+                  },
+                  icon: Icon(icon), 
+                  label: Text('Like'),
+                  ),
+                ElevatedButton(
+                  onPressed: () {
+                    print('Hello');
+                    appState.getNext();
+                  },
+                  child: Text('Next'),
+                ),
+              ],
             ),
           ],
         ),
